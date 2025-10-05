782bf2d0a2362e68e82d2de2f612d8306d30fbfd
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-10-05 14:20:09 +0530

    adding dev dependencies for deployment

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -10,14 +10,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 dotenv.config();
 app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true 
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
 }));
 app.use(cookieParser());
 dbConnect();
 
 app.use("/api/v1",router);
 
-app.listen(process.env.PORT,()=>{
-  console.log("Server is runing")
-})
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
